0ddbe12bcc571cfffa2c1536fddaa86f355c89f3
Wagner Canto <wcanto46@gmail.com>
2026-06-12 22:39:38 -0300

    fix: corrigir encoding UTF-8 na saída do console

--- check_rm_awards.py
+++ check_rm_awards.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Lista os valores distintos de DEGREE_AWARD na tabela TMP_SYMPLICITY do RM."""
 import sys
+sys.stdout.reconfigure(encoding="utf-8")
 import pyodbc
 from config import RM_CONN_STR
 
